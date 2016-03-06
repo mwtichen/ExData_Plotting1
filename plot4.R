60b71e86a36d6ef66ede01567b8cb476f3fdea79
mwtichen <mwtichenor@gmail.com>
2016-03-06 10:16:23 -0600

    fixed the legend

--- plot4.R
+++ plot4.R
@@ -13,7 +13,7 @@ with(data,plot(datetime,Sub_metering_1,type="n",xlab='',ylab='Energy sub meterin
 points(data$datetime,data$Sub_metering_1,type='l')
 points(data$datetime,data$Sub_metering_2,type='l',col='red')
 points(data$datetime,data$Sub_metering_3,type='l',col='blue')
-legend('topright',lty=1,col=c('black','red','blue'),legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),cex=0.6)
+legend('topright',lty=1,col=c('black','red','blue'),legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),cex=0.6,bty="n")
 with(data,plot(datetime,Global_reactive_power,type='l'))
 dev.copy(png,file="plot4.png")
 dev.off()
